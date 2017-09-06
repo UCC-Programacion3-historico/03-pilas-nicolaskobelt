4788a57db8d466ecf527ecf1d4d88fc3bc0ec71e
nicolaskobelt <kobeltnicolas@me.com>
2017-09-06 19:36:31 -0300

    Ej 1 al 50%

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include "../Pila/Pila.h"
 
+using namespace std;
+
 int main() {
+    char palabra[10];
+    cin>>palabra;
+
+    Pila<char> inv;
+
+    unsigned l = strlen(palabra);
+    for(int i=0; i<l; i++){
+        inv.push(palabra[i]);
+    }
+
     std::cout << "Ejercicio 03/01\n" << std::endl;
     return 0;
 }
